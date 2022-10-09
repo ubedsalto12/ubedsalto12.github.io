fbdaa96d5ba7afb7fbfa0b6126ec90e82d79af00
ubedsalto12 <65477300+ubedsalto12@users.noreply.github.com>
2022-10-09 19:47:55 +0800

    Add files via upload

---
+++ POSTTEST 3/tes.js
@@ -0,0 +1,16 @@
+const checkbox = document.getElementById('checkbox1');
+checkbox.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+});
+
+alert("Selamat Datang Di YOK OLAHRAGA")
+
+const nama = document.getElementById('nama');
+nama.innerHTML = 'Nama : Muhammad Reza Ubaidillah';
+
+const nim = document.getElementById('nim');
+nim.innerHTML = 'NIM : 2109106084';
+
+const kelas = document.getElementById('kelas');
+kelas.innerHTML = 'Kelas : Informatika B 2 2021';
+
